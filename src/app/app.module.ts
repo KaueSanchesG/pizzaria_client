a6e3352e9b114f1e88af8105a399c92266737c43
ppedroandrade <pedrosalvaro09@gmail.com>
2023-10-18 16:02:57 -0300

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { IndexComponent } from './layout/index/index.component';
 
 @NgModule({
-  declarations: [AppComponent, ClienteListComponent],
+  declarations: [AppComponent, ClienteListComponent, HeaderComponent, FooterComponent, IndexComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
