bdd71de66d7552ad6e84438db5e2f14354c135ea
KaueSanchesG <kauesanches123@gmail.com>
2023-10-21 10:45:30 -0300

    refactor: updating models

--- src/app/models/produto-sabor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProdutoSabor } from './produto-sabor';
-
-describe('ProdutoSabor', () => {
-  it('should create an instance', () => {
-    expect(new ProdutoSabor()).toBeTruthy();
-  });
-});
