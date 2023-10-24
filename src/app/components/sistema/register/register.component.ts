3d7168c4675b404f0e3410841adaaf737dde23b8
KaueSanchesG <kauesanches123@gmail.com>
2023-10-24 11:50:12 -0300

    feat: funcionario create working

--- src/app/components/sistema/register/register.component.ts
+++ src/app/components/sistema/register/register.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Funcionario } from 'src/app/models/funcionario';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private router: Router
+  ) {}
 
+  funcionario: Funcionario = new Funcionario();
+
+  salvar() {
+    this.funcionarioService
+      .create(this.funcionario)
+      .then((response) => {
+        console.log(`Funcionário ${response.data.nome} criado com sucesso`);
+        this.router.navigate(['/']);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
