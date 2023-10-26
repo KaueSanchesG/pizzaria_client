8ff75b61136ed10261f1cfc682d87273b7bb7756
KaueSanchesG <kauesanches123@gmail.com>
2023-10-26 09:41:39 -0300

    feat: integrating the backend

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PedidoService } from 'src/app/services/pedido.service';
 import { Pedido } from 'src/app/models/pedido';
+import { Produto } from 'src/app/models/produto';
+import { Pizza } from 'src/app/models/pizza';
 
 @Component({
   selector: 'app-order',
@@ -9,7 +11,12 @@ import { Pedido } from 'src/app/models/pedido';
   styleUrls: ['./order.component.scss'],
 })
 export class OrderComponent {
+  list: Pedido[] = [];
+
   pedidoSelecionado: Pedido = new Pedido();
+
+  produtosDoPedido: Produto[] = [];
+  pizzasDoPedido: Pizza[] = [];
 
   constructor(private modal: NgbModal, private service: PedidoService) {
     this.listAll();
@@ -18,7 +25,10 @@ export class OrderComponent {
   listAll() {
     this.service
       .list()
-      .then((response) => {})
+      .then((response) => {
+        this.list = response.data;
+        console.log(response.data);
+      })
       .catch((error) => {
         console.log(error);
       });
@@ -26,6 +36,12 @@ export class OrderComponent {
 
   openNewOrderModal(modal: any) {
     this.pedidoSelecionado = new Pedido();
+
+    this.modal.open(modal, { size: 'lg' });
+  }
+  openDetailsModal(modal: any, pedido: Pedido) {
+    this.produtosDoPedido = pedido.produtoList;
+    this.pizzasDoPedido = pedido.pizzaList;
 
     this.modal.open(modal, { size: 'lg' });
   }
